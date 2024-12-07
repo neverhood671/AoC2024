30125969c4c1a95f905d2910a13835fdc41d30cc
Anastasiia Mishunina <anastasiia.mishunina@klarna.com>
2024-12-07 19:30:20 +0100

    add day7 part2 solution

--- day7/day7.js
+++ day7/day7.js
@@ -50,4 +50,46 @@ const solvePart1 = (input) => {
     }, 0)
 }
 
-console.log(solvePart1(readAllStringsFromFile('./input.txt')));
+
+const isEquationCorrectV2 = (equation) => {
+    const { test, numbers } = equation;
+
+    if (numbers.length === 2)
+        return numbers[0] * numbers[1] === test
+            || numbers[0] + numbers[1] === test
+            || `${numbers[0]}${numbers[1]}` === `${test}`;
+
+    const lastNumber = numbers[numbers.length - 1];
+    const newNumbers = numbers.slice(0, numbers.length - 1);
+
+    const doesDivisionWork = ((test % lastNumber === 0) && isEquationCorrectV2({
+        test: test / lastNumber,
+        numbers: newNumbers,
+    }));
+
+    const doesSubtractionWork = ((test - lastNumber > 0) && isEquationCorrectV2({
+        test: test - lastNumber,
+        numbers: newNumbers,
+    }));
+
+    const testStr = `${test}`;
+    const lastNumberStr = `${lastNumber}`;
+
+    const doesConcatenationWork = testStr.endsWith(lastNumberStr) && isEquationCorrectV2({
+        test: parseInt(testStr.slice(0, testStr.length - lastNumberStr.length)),
+        numbers: newNumbers,
+    });
+    return doesDivisionWork || doesSubtractionWork || doesConcatenationWork;
+}
+
+const solvePart2 = (input) => {
+    const equations = getEquations(input);
+
+    return equations.reduce((acc, curr) => {
+        const isCorrect = isEquationCorrectV2(curr);
+        if (isCorrect) acc += curr.test;
+        return acc;
+    }, 0)
+}
+
+console.log(solvePart2(readAllStringsFromFile('./input.txt')));
