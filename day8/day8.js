42a96b1269b2b3bd0fc3e40a8934856769be62f7
Anastasiia Mishunina <anastasiia.mishunina@klarna.com>
2024-12-08 15:40:34 +0100

    add day8 part2 solution

--- day8/day8.js
+++ day8/day8.js
@@ -36,7 +36,6 @@ const getDistance = (p1, p2) => {
     return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
 }
 
-
 const solvePart1 = (input) => {
     const map = input.map(s => s.split(''));
     const antennas = getAllAntennasCoordinates(map);
@@ -74,4 +73,47 @@ const solvePart1 = (input) => {
     return antiNodes.size;
 }
 
-console.log(solvePart1(readAllStringsFromFile('./input.txt')));
+const getSquareDistance = (p1, p2) => {
+    const [x1, y1] = p1;
+    const [x2, y2] = p2;
+
+    return (x2 - x1) ** 2 + (y2 - y1) ** 2;
+}
+
+
+const solvePart2 = (input) => {
+    const map = input.map(s => s.split(''));
+    const antennas = getAllAntennasCoordinates(map);
+
+    const antiNodes = new Set();
+
+    for (let i = 0; i < antennas.length-1; i++) {
+        for (let j = i+1; j < antennas.length; j++) {
+         
+            const [x1, y1] = antennas[i];
+            const [x2, y2] = antennas[j];
+
+            if (map[x1][y1] !== map[x2][y2]) continue;
+
+            antiNodes.add([x1, y1].join(','));
+            antiNodes.add([x2, y2].join(','));
+            
+            for (let k = 0; k < map.length; k++) {
+                for (let l = 0; l < map[0].length; l++) {
+                   if (!doPointsLayInOneLine(antennas[i], antennas[j], [k, l])) continue;
+
+                    const antennasManhattanDistance = getSquareDistance(antennas[i], antennas[j]);
+                    const firstAntennaManhattanDistance = getSquareDistance([k, l], antennas[i]);
+     
+                    if (firstAntennaManhattanDistance % antennasManhattanDistance === 0) {
+                        antiNodes.add([k, l].join(','));
+                    }
+                }
+            }
+        }
+    }
+
+    return antiNodes.size;
+}
+
+console.log(solvePart2(readAllStringsFromFile('./input.txt')));
