e12d54f37b0aa256b48988fb52e455f12cbd806f
Anastasiia Mishunina <anastasiia.mishunina@klarna.com>
2024-12-08 14:47:34 +0100

    add solution day8 part1

---
+++ day8/day8.js
@@ -0,0 +1,77 @@
+const fs = require('fs');
+
+
+function readAllStringsFromFile(filePath) {
+    return fs
+        .readFileSync(filePath)
+        .toString('utf-8')
+        .split('\n');
+}
+
+
+
+
+const getAllAntennasCoordinates = (map) => {
+    const res = [];
+    for (let i = 0; i < map.length; i++) {
+        for (let j = 0; j < map[0].length; j++) {
+            if (map[i][j] !== '.') res.push([i, j]);
+        }
+    }
+    return res;
+}
+
+const doPointsLayInOneLine = (p1, p2, p3) => {
+    const [x1, y1] = p1;
+    const [x2, y2] = p2;
+    const [x3, y3] = p3;
+
+    return (x3 - x1) * (y2 - y1) === (y3 - y1) * (x2 - x1);
+}
+
+const getDistance = (p1, p2) => {
+    const [x1, y1] = p1;
+    const [x2, y2] = p2;
+
+    return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+}
+
+
+const solvePart1 = (input) => {
+    const map = input.map(s => s.split(''));
+    const antennas = getAllAntennasCoordinates(map);
+
+    const antiNodes = new Set();
+
+    for (let i = 0; i < antennas.length-1; i++) {
+        for (let j = i+1; j < antennas.length; j++) {
+         
+            const [x1, y1] = antennas[i];
+            const [x2, y2] = antennas[j];
+
+            if (map[x1][y1] !== map[x2][y2]) continue;
+            
+            for (let k = 0; k < map.length; k++) {
+                for (let l = 0; l < map[0].length; l++) {
+                   if (k === x1 && l === y1) continue;
+                   if (k === x2 && l === y2) continue;
+                   if (!doPointsLayInOneLine(antennas[i], antennas[j], [k, l])) continue;
+
+                    const firstAntennaDistance = getDistance([k, l], antennas[i]);
+                    const secondAntennaDistance = getDistance([k, l], antennas[j]);
+
+                    if (firstAntennaDistance === 2* secondAntennaDistance || secondAntennaDistance === 2* firstAntennaDistance) {
+                        antiNodes.add([k, l].join(','));
+                    }
+
+                }
+            }
+
+            
+        }
+    }
+
+    return antiNodes.size;
+}
+
+console.log(solvePart1(readAllStringsFromFile('./input.txt')));
